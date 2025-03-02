83527556e4f5691f21fa3ed6707bfcb839a83f2b
martinmonzo <martinmgancia1@gmail.com>
2025-03-02 16:44:50 -0300

    Add service tests and fix: falsy not None values for provider2

---
+++ backend/properties/tests/mock.py
@@ -0,0 +1,27 @@
+PROPERTY_PROVIDER_1_MOCKED = {
+    "formattedAddress": "123 Main St, Anytown, USA",
+    "squareFootage": 2000,
+    "lotSizeSqFt": 1.5,
+    "yearBuilt": 1990,
+    "propertyType": "House",
+    "bedrooms": 3,
+    "bathrooms": 2,
+    "lastSalePrice": 250000,
+    "features": {
+        "roomCount": 3,
+        "septicSystem": True,
+    }
+}
+
+PROPERTY_PROVIDER_2_MOCKED = {
+    "NormalizedAddress": "123 Main Street, Anytown, USA",
+    "SquareFootage": 1900,
+    "LotSizeAcres": 1.6,
+    "YearConstructed": 1992,
+    "PropertyType": "Single Family Home",
+    "Bedrooms": 3,
+    "Bathrooms": 2,
+    "SalePrice": 245000,
+    "RoomCount": 2,
+    "SepticSystem": False,
+}
